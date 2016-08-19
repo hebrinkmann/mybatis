ec65c04fbfd887ad118d493c6f610eb04f766081
Henning Brinkmann <Henning.Brinkmann@evodion.de>
2016-08-19 17:46:12 +0200

    AppConfig: gitProperties

--- src/main/java/de/henningBrinkmann/mybatisSample/config/AppConfig.java
+++ src/main/java/de/henningBrinkmann/mybatisSample/config/AppConfig.java
@@ -1,8 +1,15 @@
 package de.henningBrinkmann.mybatisSample.config;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,6 +20,7 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan(basePackages = "de.henningBrinkmann.mybatisSample")
 public class AppConfig extends WebMvcConfigurerAdapter {
+	private static Logger logger = LoggerFactory.getLogger(AppConfig.class);
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -22,5 +30,16 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
+	}
+	
+	@Bean
+	public Properties gitProperties() {
+		try {
+			return PropertiesLoaderUtils.loadProperties(new ClassPathResource("/git.properties"));
+		} catch (IOException e) {
+			logger.error("Failed to load git.properties: {}", e);
+		}
+		
+		return new Properties();
 	}
 }
