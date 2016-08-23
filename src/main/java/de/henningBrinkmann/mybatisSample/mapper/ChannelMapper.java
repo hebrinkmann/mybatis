ef2f40c53d51c8254e53570e1c431b0095be4a37
Henning Brinkmann <Henning.Brinkmann@evodion.de>
2016-08-23 14:01:40 +0200

    ChannelMapper

---
+++ src/main/java/de/henningBrinkmann/mybatisSample/mapper/ChannelMapper.java
@@ -0,0 +1,15 @@
+package de.henningBrinkmann.mybatisSample.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+import de.henningBrinkmann.mybatisSample.xmltv.Channel;
+
+public interface ChannelMapper {
+	void insertChannel(Channel channel);
+	
+	@Select("SELECT * FROM mybatissample.channel WHERE `idchannel` = #{id};")
+	Channel getChannel(int id);
+
+	@Select("SELECT * FROM mybatissample.channel WHERE `displayname` = #{displayName};")
+	Channel getChannelByDisplayName(String displayName);
+}
