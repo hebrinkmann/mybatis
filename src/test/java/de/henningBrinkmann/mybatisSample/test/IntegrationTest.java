60bfcab4966bbe041703b048d4129f83dc46633e
Henning Brinkmann <Henning.Brinkmann@evodion.de>
2016-08-23 13:57:41 +0200

    IntegrationTest: @Transactional

--- src/test/java/de/henningBrinkmann/mybatisSample/test/IntegrationTest.java
+++ src/test/java/de/henningBrinkmann/mybatisSample/test/IntegrationTest.java
@@ -1,17 +1,17 @@
 package de.henningBrinkmann.mybatisSample.test;
 
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.henningBrinkmann.mybatisSample.config.AppConfig;
-import de.henningBrinkmann.mybatisSample.util.InjectLogger;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 @ComponentScan(basePackages = "de.henningBrinkmann.mybatisSample")
+@Transactional
 public abstract class IntegrationTest {
 	public IntegrationTest() {
 		super();
