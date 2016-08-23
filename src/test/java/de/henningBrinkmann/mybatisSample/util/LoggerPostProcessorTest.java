0ba6b02acc975f8e29e7ff659ed8ffe645c594be
Henning Brinkmann <Henning.Brinkmann@evodion.de>
2016-08-23 13:55:37 +0200

    LoggerPostProcessorTest: Autowiring does not work in test class. Use ClassWithLogger.

--- src/test/java/de/henningBrinkmann/mybatisSample/util/LoggerPostProcessorTest.java
+++ src/test/java/de/henningBrinkmann/mybatisSample/util/LoggerPostProcessorTest.java
@@ -1,18 +1,18 @@
 package de.henningBrinkmann.mybatisSample.util;
 
 import org.junit.Test;
-import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 
 import de.henningBrinkmann.mybatisSample.test.WebIntegrationTest;
 
 public class LoggerPostProcessorTest extends WebIntegrationTest {
-	@InjectLogger
-	protected Logger logger;
-
+	@Autowired
+	private ClassWithLogger testee;
+	
 	@Test
 	public void loggerSouldBeInjected() {
-		Assert.notNull(logger, "Logger should not be null.");
+		Assert.notNull(testee.getLogger(), "Logger should not be null.");
 	}
 
 }
