278503f00e21fa84ca4c89e0d55bb2c77e9388a6
Henning Brinkmann <Henning.Brinkmann@evodion.de>
2016-08-23 15:37:48 +0200

    Title: setters

--- src/main/java/de/henningBrinkmann/mybatisSample/xmltv/Title.java
+++ src/main/java/de/henningBrinkmann/mybatisSample/xmltv/Title.java
@@ -4,17 +4,25 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlValue;
 
 public class Title {
-	public String getValue() {
-		return value;
-	}
+	@XmlValue
+	private String value;
+
+	@XmlAttribute
+	private String lang;
 
 	public String getLang() {
 		return lang;
 	}
 
-	@XmlValue
-	private String value;
-
-	@XmlAttribute
-	private String lang;	
+	public String getValue() {
+		return value;
+	}	
+	
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
