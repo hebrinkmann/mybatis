24ad63a1282706330fcd24916f07896cd602602d
Henning Brinkmann <Henning.Brinkmann@evodion.de>
2016-08-23 18:01:52 +0200

    Description: setters

--- src/main/java/de/henningBrinkmann/mybatisSample/xmltv/Description.java
+++ src/main/java/de/henningBrinkmann/mybatisSample/xmltv/Description.java
@@ -9,18 +9,29 @@ public class Description {
 	
 	@XmlAttribute
 	private String lang;
+	
+	private Integer iddescription;
+
+	public Integer getIddescription() {
+		return iddescription;
+	}
 
 	public String getLang() {
 		return lang;
 	}
-
+	
 	public String getValue() {
 		return value;
+	}
+	
+	public void setIddescription(Integer iddescription) {
+		this.iddescription = iddescription;
 	}
 	
 	public void setLang(String lang) {
 		this.lang = lang;
 	}
+	
 	public void setValue(String value) {
 		this.value = value;
 	}
